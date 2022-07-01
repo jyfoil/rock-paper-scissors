750b1ae8b3a5b7be013e27fde63f448cdf09dfb7
Justin Yang <yjust16@gmail.com>
2022-06-30 21:05:00 -0700

    Add playerSelection function
    Prompts the user to enter either Rock, Paper, or Scissors. Depending on
    the choice it will return the corresponding number, either 0, 1, or 2.

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ function computerPlay() {
     return "Scissors";
   }
 }
+
+function playerSelection() {
+  let choice = prompt("Rock, Paper or Scissors?");
+  if (choice === "Rock") {
+    return 0;
+  } else if (choice === "Paper") {
+    return 1;
+  } else if (choice === "Scissors") {
+    return 2;
+  } else {
+    alert("Choose Rock, Paper, or Scissors!");
+  }
+}
