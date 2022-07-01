944d1679bef05218270d32f6e718a939ba3a629f
Justin Yang <yjust16@gmail.com>
2022-07-01 15:30:31 -0700

    Add logs to functions

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function computerPlay() {
   const randomChoice = Math.floor(Math.random() * 3);
   if (randomChoice === 0) {
+    console.log("Computer chose Rock!");
     return "Rock";
   } else if (randomChoice === 1) {
+    console.log("Computer chose Paper!");
     return "Paper";
   } else {
+    console.log("Computer chose Scissors!");
     return "Scissors";
   }
 }
@@ -12,12 +15,15 @@ function computerPlay() {
 function playerSelection() {
   let choice = prompt("Rock, Paper or Scissors?");
   if (choice === "Rock") {
+    console.log("You chose Rock!");
     return 0;
   } else if (choice === "Paper") {
+    console.log("You chose Paper!");
     return 1;
   } else if (choice === "Scissors") {
+    console.log("You chose Scissors!");
     return 2;
   } else {
-    alert("Choose Rock, Paper, or Scissors!");
+    alert("Please choose Rock, Paper, or Scissors!");
   }
 }
