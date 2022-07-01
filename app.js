7803e5dea0dfbc0ae011dc1180957cc9741eb43b
Justin Yang <yjust16@gmail.com>
2022-06-30 19:15:40 -0700

    Add computerPlay function
    Added a function that generates a random number between 0-2. The number
    0 represents rock, 1 represents paper, and 2 represents scissors. Which-
    ever number is chosen it returns the corresponding sign.

---
+++ app.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+  const randomChoice = Math.floor(Math.random() * 3);
+  if (randomChoice === 0) {
+    return "Rock";
+  } else if (randomChoice === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
