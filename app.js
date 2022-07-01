c03b1dab3e2dc2617e3ca280d13e530cb4b6d7c7
Justin Yang <yjust16@gmail.com>
2022-07-01 15:56:37 -0700

    Add playRound function
    Prompts user to enter either Rock, Paper, or Scissors. Computer randomly
    chooses between the three, depending on the choices, either win, lose,
    or tie.

--- app.js
+++ app.js
@@ -27,3 +27,23 @@ function playerSelection() {
     alert("Please choose Rock, Paper, or Scissors!");
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("Nobody wins! Tied!");
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    console.log("You lose! Paper beats Rock!");
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    console.log("You lose! Scissors beats Paper!");
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    console.log("You lose! Rock beats Scissors!");
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    console.log("You win! Rock beats scissors!");
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    console.log("You win! Paper beats Rock!");
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    console.log("You win! Scissors beats Paper!");
+  }
+}
+
+playRound(playerSelection(), computerPlay());
