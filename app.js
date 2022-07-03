51c08dee650a1c9728a18b18891ff91e5fd0fd69
Justin Yang <yjust16@gmail.com>
2022-07-02 17:56:33 -0700

    Change logs to return values

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log("Nobody wins! Tied!");
+    return "Nobody wins! Tied!";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("You lose! Paper beats Rock!");
+    return "You lose! Paper beats Rock!";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("You lose! Scissors beats Paper!");
+    return "You lose! Scissors beats Paper!";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("You lose! Rock beats Scissors!");
+    return "You lose! Rock beats Scissors!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("You win! Rock beats scissors!");
+    return "You win! Rock beats scissors!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You win! Paper beats Rock!");
+    return "You win! Paper beats Rock!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("You win! Scissors beats Paper!");
+    return "You win! Scissors beats Paper!";
   }
 }
 
-playRound(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection));
