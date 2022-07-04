95c1faf98daea88adef2cc22a94c4a9be63755c7
Justin Yang <yjust16@gmail.com>
2022-07-03 19:20:03 -0700

    Add score with winner message

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   const randomChoice = Math.floor(Math.random() * 3);
   if (randomChoice === 0) {
@@ -13,16 +16,22 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "Nobody wins! Tied!";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     return "You lose! Paper beats Rock!";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     return "You lose! Scissors beats Paper!";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     return "You lose! Rock beats Scissors!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
     return "You win! Rock beats scissors!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
     return "You win! Paper beats Rock!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return "You win! Scissors beats Paper!";
   }
 }
@@ -32,6 +41,14 @@ function game() {
     const playerSelection = prompt("Rock, Paper or Scissors?")?.toLowerCase();
     const computerSelection = computerPlay();
     console.log(playRound(playerSelection, computerSelection));
+    console.log(`Player score ${playerScore} Computer Score: ${computerScore}`);
+  }
+  if (playerScore < computerScore) {
+    console.log("Computer wins");
+  } else if (playerScore > computerScore) {
+    console.log("Player wins");
+  } else {
+    console.log("Tied!");
   }
 }
 
